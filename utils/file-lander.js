016fb35d1eaa3d0a49685efcbb3afdbaeee2a0f6
Cumarcosoble12 <cumarmuxumed@icloud.com>
2024-11-02 01:34:06 -0700

    mnb

--- utils/file-lander.js
+++ utils/file-lander.js
@@ -3,13 +3,13 @@ const path = require('path');
 
 const filePath = './data/tasks.json';
 
-exports.writeTasksToFile = (tasks) => {
-    if (Array.isArray(tasks) && tasks.length === 0) {
-        fs.writeFileSync(filePath, '[]');
-    } else {
-        fs.writeFileSync(filePath, JSON.stringify(tasks, null, 2));
-    }
-}
+// exports.writeTasksToFile = (tasks) => {
+//     if (Array.isArray(tasks) && tasks.length === 0) {
+//         fs.writeFileSync(filePath, '[]');
+//     } else {
+//         fs.writeFileSync(filePath, JSON.stringify(tasks, null, 2));
+//     }
+// }
 
 exports.readTasksFromFile = () => {
     if(!fs.existsSync(filePath)) {
